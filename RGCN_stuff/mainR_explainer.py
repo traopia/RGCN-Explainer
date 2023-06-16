d85ebf8ea5905ffe0195014d4ed5132b94f44bc9
Teresa Liberatore <terryliberatore@gmail.com>
2023-06-16 17:18:02 +0200

    Update mainR_explainer.py

--- RGCN_stuff/mainR_explainer.py
+++ RGCN_stuff/mainR_explainer.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 #import kgbench as kg
 
-from src.load import load
+from kgbench import load, tic, toc, d
 #rgcn 
 from rgcn import  RGCN
 from src.rgcn_explainer_utils import *
