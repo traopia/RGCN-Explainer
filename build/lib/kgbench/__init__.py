739dc2e72c4d9f523aae1c0ca85f2c6c45861617
Teresa Liberatore <terryliberatore@gmail.com>
2023-09-12 11:10:54 +0200

    reordering

--- build/lib/kgbench/__init__.py
+++
@@ -1,6 +0,0 @@
-from .load import load, Data, prune, group, datatype_key
-
-from .util import load_rdf, tic, toc, d, to_tensorbatch, to_tensorbatches, \
-    to_tvbatches, to_tvbatch, entity, entity_hdt, n3, getfile, here
-
-from .parse import parse_term, Resource, Entity, Literal, BNode, IRIRef
